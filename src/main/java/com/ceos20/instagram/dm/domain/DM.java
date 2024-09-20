723b6f2975ca98c089d900691e8467cb25b1d366
jimin <010709min@ewhain.net>
2024-09-20 13:27:34 +0900

    [feat] DM 관련 entity 생성

---
+++ src/main/java/com/ceos20/instagram/dm/domain/DM.java
@@ -0,0 +1,45 @@
+package com.ceos20.instagram.dm.domain;
+
+import com.ceos20.instagram.user.domain.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class DM {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dm_int")
+    private Long id;
+
+    // 내용
+    @Column(nullable = false)
+    private String content;
+
+    // 전송일
+    @Column(nullable = false)
+    private LocalDateTime sendedAt = LocalDateTime.now()
+
+    // 읽음 여부
+    @Column(nullable = false)
+    private boolean isRead = false;
+
+    // 공감
+    @Column(length = 1)
+    private String emotion;
+
+    // 보낸 사용자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private User senderID;
+
+    // 받는 사용자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User receiverID;
+
+    // DM방
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private DMRoom dmRoomID;
+}
---
+++ src/main/java/com/ceos20/instagram/dm/domain/DMRoom.java
@@ -0,0 +1,25 @@
+package com.ceos20.instagram.dm.domain;
+
+import com.ceos20.instagram.user.domain.User;
+import jakarta.persistence.*;
+
+@Entity
+public class DMRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long id;
+
+    // 대화에 참여하는 유저1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User userID1;
+
+    // 대화에 참여하는 유저2
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User userID2;
+
+    @Column(nullable = false)
+    private Long restChatCount= 0L;
+}
