cfca0167d108c2e1d68873b6de5a2e70d3ca4d6d
jimin <010709min@ewhain.net>
2024-11-03 02:15:49 +0900

    [fix] follow 오타 수정

--- src/main/java/com/ceos20/instagram/user/controller/FollowController.java
+++ src/main/java/com/ceos20/instagram/user/controller/FollowController.java
@@ -1,6 +1,10 @@
 package com.ceos20.instagram.user.controller;
 
+import com.ceos20.instagram.user.dto.FollowCreateDto;
+import com.ceos20.instagram.user.dto.FollowersResponseDto;
+import com.ceos20.instagram.user.dto.FollowingsResponseDto;
 import com.ceos20.instagram.user.service.FollowService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,20 +19,20 @@ public class FollowController {
     private final FollowService followService;
 
     @PostMapping("/{nickname}")
-    public ResponseEntity createFollow(@PathVariable String nickname, @RequestParam("user") String user) {
+    public ResponseEntity createFollow(@RequestBody @Valid FollowCreateDto followCreateDto, @PathVariable String nickname, @RequestParam("user") String user) {
         followService.createFollow(followCreateDto, nickname, user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/{nickname}/followings")
     public ResponseEntity getFollowings(@PathVariable String nickname) {
-        final List<FollowingResponseDto> response = followService.getFollowing(nickname);
+        final List<FollowingsResponseDto> response = followService.getFollowing(nickname);
         return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/{nickname}/followers")
     public ResponseEntity getFollowers(@PathVariable String nickname) {
-        final List<FollowerResponseDto> response = followService.getFollower(nickname);
+        final List<FollowersResponseDto> response = followService.getFollower(nickname);
         return ResponseEntity.ok().body(response);
     }
 }
